3598ab893b9a7170d84861b300f819d62556e1f7
juimin <10312217+juimin@users.noreply.github.com>
2026-03-07 09:34:33 +0900

    asdf

---
+++ rust/eulerlib/src/primes.rs
@@ -0,0 +1,45 @@
+pub fn find_primes_sieve(n: u64) -> Vec<u64> {
+    // find all primes using the unoptimized sieve method
+    let vec_size = ((n + 1) as f64).sqrt().round() as usize;
+    let mut potential_primes: Vec<bool> = vec![true; vec_size];
+
+    println!("FInding potential primes in {}", n);
+
+    for num in 2..vec_size {
+        let is_potential_prime: bool = potential_primes[num];
+
+        if is_potential_prime && is_prime_trial_div(num as u64) {
+            let mut tmp = num * 2;
+            while tmp < (vec_size as usize) {
+                potential_primes[tmp] = false;
+                tmp += num;
+            }
+        }
+    }
+
+    println!("Finished finding potential primes");
+
+    let mut primes: Vec<u64> = Vec::new();
+    for i in 1..vec_size - 1 {
+        if potential_primes[i] {
+            primes.push(i as u64);
+        }
+    }
+
+    println!("Found {} primes", primes.len());
+
+    return primes
+
+}
+
+pub fn is_prime_trial_div(n: u64) -> bool {
+    let mut val = 2;
+    while (val * val) <= n {
+        if n % val == 0 {
+            return false
+        }
+        val += 1
+    }
+
+    return true
+}
